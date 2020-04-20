af37a4f001ec5596509b18c517f5b8f5dbaea161
Thomas Niederberger <thomas@niederb.ch>
2020-04-20 10:29:55 +0200

    Fix block colors

--- scenes/Block.gd
+++ scenes/Block.gd
@@ -5,13 +5,16 @@ func modulate_color(new_color_index):
 	apply_color()
 	
 func apply_color():
-	var color = Globals.colors[color_index]
-	$Sprite.self_modulate = color
-	$Light.color = color
+	if color_index >= 0:
+		var color = Globals.colors[color_index]
+		$Sprite.self_modulate = color
+		#$Sprite.modulate = color
+		$Light.color = color
+	else:
+		$Light.enabled = false
 
 func die():
 	queue_free()
-
 
 func _on_Block_body_entered(body):
 	get_tree().call_group("Gamestate", "game_over")
