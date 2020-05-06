86d5c44042883646171be0d37b4bc3e68409cbe1
Thomas Niederberger <thomas@niederb.ch>
2020-05-06 16:23:19 +0200

    Work on map selection

--- scenes/LevelConfig.gd
+++ scenes/LevelConfig.gd
@@ -5,7 +5,7 @@ var n_goodies: int = 3
 var min_combo_size: int = 3
 var movement_speed: float = 4.0
 var start_cell := Vector2()
-var map_name: String = ""
+var map_name: String = "RectangleMap"
 
 func get_time_interval():
 	return 1.0 / movement_speed
--- scenes/Menu/Options.gd
+++ scenes/Menu/Options.gd
@@ -5,7 +5,7 @@ func _ready():
 	
 func update_GUI() -> void:
 	for i in $Level.get_item_count():
-		if $Level.get_item_text() == Globals.level_name:
+		if $Level.get_item_text(i) == Globals.level_config.map_name:
 			$Level.select(i)
 	
 	$NumberColors.text = str(Globals.level_config.n_colors)
