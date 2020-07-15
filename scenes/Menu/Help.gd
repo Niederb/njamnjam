33155d4a32194e1864b8259f996d194e6d46223e
Thomas Niederberger <thomas@niederb.ch>
2020-07-15 15:06:12 +0200

    Add rudamentary help

---
+++ scenes/Menu/Help.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_MainMenuButton_pressed():
+	Globals.change_scene("res://scenes/Menu/MainMenu.tscn")
--- scenes/Menu/MainMenu.gd
+++ scenes/Menu/MainMenu.gd
@@ -6,6 +6,12 @@ func _on_Campaign_pressed():
 func _on_IndividualGame_pressed():
 	Globals.change_level("res://scenes/Menu/IndividualGame.tscn")
 
+func _on_Help_pressed():
+	Globals.change_level("res://scenes/Menu/Help.tscn")
+	
+func _on_Credits_pressed():
+	Globals.change_level("res://scenes/Menu/Credits.tscn")	
+
 func _on_Exit_pressed():
 	get_tree().quit(0)
 
