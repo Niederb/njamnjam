5d2b18598d408994f6c94bbba05ed17212846813
Thomas Niederberger <thomas@niederb.ch>
2020-07-16 07:56:11 +0200

    Work on level 12

--- scenes/Levels/Level12.gd
+++ scenes/Levels/Level12.gd
@@ -3,10 +3,13 @@ extends "res://scenes/LevelTemplate.gd"
 func _ready():
 	Globals.level_config.n_colors = 3
 	Globals.level_config.n_goodies = 3
-	Globals.level_config.map_name = "FourPillarsMap"
+	Globals.level_config.map_name = "MarinaMap"
 	level_number = 12
-	$WinCondition.config.score = 200
+	$WinCondition.config.remove_all_blocks = true
 	start_game()
 
 func get_success_text():
 	return "Campaign finished. \n !!Congratulations!! \n Going back to main menu..."
+
+func get_tutorial_text():
+	return "Removing black blocks\n\nRemove the black block by using the separators."
